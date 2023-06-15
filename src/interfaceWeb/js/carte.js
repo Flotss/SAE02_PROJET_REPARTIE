0366531c4a4a842df7be542a2430671eeb4ef2ea
Thomas <robineau1u@etu.univ-lorraine.fr>
2023-06-15 16:32:50 +0200

    Logo incidents circulation

--- src/interfaceWeb/js/carte.js
+++ src/interfaceWeb/js/carte.js
@@ -1,6 +1,7 @@
 import restaurant from "./restaurant.js";
 import uiReservation from "./uiReservation.js";
 import {getBikeAvailability, getStationAvailability, getStationData} from "../../trafficInformations/VelostanNancy.js";
+import {getCirculationIncidents} from "../../trafficInformations/CirculationIncidents.js";
 
 console.log('Hi map ! ');
 
@@ -13,26 +14,34 @@ const zoomLevel = 12;
 const GroupeMarkerResto = L.layerGroup([]);
 const GroupeMarkerVlib = L.layerGroup([]);
 const GroupeMarkerEtablissementEnsSup = L.layerGroup([]);
+const GroupeMarkerIncidents = L.layerGroup([]);
 
 const iconVlib = L.icon({
     iconUrl: 'stylesheet/image/logoVelib.png',
-    iconSize:     [40, 40], // size of the icon
-    iconAnchor:   [20, 38], // point of the icon which will correspond to marker's location
-    popupAnchor:  [1, -30] // point from which the popup should open relative to the iconAnchor
+    iconSize: [40, 40], // size of the icon
+    iconAnchor: [20, 38], // point of the icon which will correspond to marker's location
+    popupAnchor: [1, -30] // point from which the popup should open relative to the iconAnchor
 });
 
 const iconResto = L.icon({
     iconUrl: 'stylesheet/image/logoResto.png',
-    iconSize:     [40, 40], // size of the icon
-    iconAnchor:   [20, 40], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -30] // point from which the popup should open relative to the iconAnchor
-
+    iconSize: [40, 40], // size of the icon
+    iconAnchor: [20, 40], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -30] // point from which the popup should open relative to the iconAnchor
 });
+
+const iconIncident = L.icon({
+    iconUrl: 'stylesheet/image/logoIncident.png',
+    iconSize: [70, 70], // size of the icon
+    iconAnchor: [20, 40], // point of the icon which will correspond to marker's location
+    popupAnchor: [1, -30]
+})
 
 const SelecteurAffichage = {
     "Restaurants": GroupeMarkerResto,
     "Stations Vlib": GroupeMarkerVlib,
-    "Etablissements enseignement supérieurs": GroupeMarkerEtablissementEnsSup
+    "Etablissements enseignement supérieur": GroupeMarkerEtablissementEnsSup,
+    "Incidents circulation": GroupeMarkerIncidents
 };
 
 
@@ -52,7 +61,6 @@ export async function init() {
     L.control.layers(null, SelecteurAffichage).addTo(map);
 
     let stations = await getStationData();
-
     for (const station of stations) {
         let stationData = station[1];
         let bikeAvailability = await getBikeAvailability(stationData.id);
@@ -60,13 +68,19 @@ export async function init() {
         addMarkerVlib(stationData.lat, stationData.lon, stationData.name, bikeAvailability, stationAvailability, stationData.address);
     }
 
+    let incidents = await getCirculationIncidents();
+    for (const incident of incidents) {
+        console.log(incident);
+        addMarkerIncidentCirculation(incident.lat, incident.lon, incident.description, incident.location, incident.start, incident.end);
+    }
+
     //A supprimé par la suite
     let lat = 48.7;
     let lng = 6.2;
     let nom = "La rivière";
     let adresse = "Dans Nancy";
-    addMarkerResto("48.71, 6.2",1,"best Resto ever", "pas important");
-    var markerResto1 = L.marker([lat, lng],{icon: iconResto});
+    addMarkerResto("48.71, 6.2", 1, "best Resto ever", "pas important");
+    var markerResto1 = L.marker([lat, lng], {icon: iconResto});
     markerResto1.bindPopup(`<b>${nom}</b><br>${adresse}`);
 
     var markerEtablissementSup = L.marker([48.69, 6.21]);
@@ -76,10 +90,10 @@ export async function init() {
     GroupeMarkerEtablissementEnsSup.addLayer(markerEtablissementSup)
 }
 
-function addMarkerResto(gps, id, nom, adresse){
+function addMarkerResto(gps, id, nom, adresse) {
     console.log(gps);
     let coordonnes = gps.split(',');
-    var marker = L.marker([coordonnes[0], coordonnes[1]],{icon: iconResto});
+    var marker = L.marker([coordonnes[0], coordonnes[1]], {icon: iconResto});
     marker.bindPopup(`<b>${nom}</b><br>${adresse}`);
     GroupeMarkerResto.addLayer(marker);
     marker.on("click", () => {
@@ -89,26 +103,32 @@ function addMarkerResto(gps, id, nom, adresse){
     });
 }
 
-function addMarkerVlib(lat, lng, nom, nbVeloDispo,nbPlaceParkingDispo , adresse){
-    var marker = L.marker([lat,lng],{icon: iconVlib});
+function addMarkerVlib(lat, lng, nom, nbVeloDispo, nbPlaceParkingDispo, adresse) {
+    var marker = L.marker([lat, lng], {icon: iconVlib});
     marker.bindPopup(`<b>${nom}</b><br>${adresse}<br>Nombre vélo dispo: ${nbVeloDispo}<br>Nombre places parking dispo: ${nbPlaceParkingDispo}`).openPopup();
     GroupeMarkerVlib.addLayer(marker);
 }
 
-function addMarkerEtablissementEnsSup(lat, lng, nom, adresse){
-    var marker = L.marker([lat,lng],{icon: iconVlib});
+function addMarkerEtablissementEnsSup(lat, lng, nom, adresse) {
+    var marker = L.marker([lat, lng], {icon: iconVlib});
     marker.bindPopup(`<b>${nom}</b><br>${adresse}`).openPopup();
     GroupeMarkerVlib.addLayer(marker);
+}
+
+function addMarkerIncidentCirculation(lat, lng, descr, adresse, start, end) {
+    let marker = L.marker([lat, lng], {icon: iconIncident});
+    marker.bindPopup(`<b>${descr}</b><br>${adresse}<br>Début: ${start}<br>Fin: ${end}`).openPopup();
+    GroupeMarkerIncidents.addLayer(marker);
 }
 
 var JsonObject
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "http://localhost:8000/api/resto", true);
-xhr.onreadystatechange = function() {
+xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
         JsonObject = JSON.parse(xhr.response);
         console.log(JsonObject);
-        for(let i = 0; i < JsonObject.restaurants.length; i++) {
+        for (let i = 0; i < JsonObject.restaurants.length; i++) {
             addMarkerResto(JsonObject.restaurants[i].GPS, JsonObject.restaurants[i].ID, JsonObject.restaurants[i].NOM, JsonObject.restaurants[i].ADRESSE);
         }
     } else {
