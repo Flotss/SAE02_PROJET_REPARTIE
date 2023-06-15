cb5b4ae684ce194d44ad875d0b63e20a1500f741
Julien Bernard <julienbernardjb88@gmail.com>
2023-06-15 11:38:11 +0200

    modif addMarker

--- untitled/src/interfaceWeb/js/carte.js
+++ untitled/src/interfaceWeb/js/carte.js
@@ -1,13 +1,13 @@
 console.log('Hi map ! ');
 
-    const nancy = {
+const nancy = {
         lat : 48.6921,
         lng : 6.1844,
     }
-    const zoomLevel = 12;
+const zoomLevel = 12;
 
-    const Resto = L.layerGroup([]);
-    const Vlib = L.layerGroup([]);
+const Resto = L.layerGroup([]);
+const Vlib = L.layerGroup([]);
 
 
 
@@ -35,10 +35,11 @@ export function init(){
     let lng = 6.2;
     let nom = "La rivière";
     let adresse = "Dans Nancy";
-
     var markerResto1 = L.marker([lat, lng]);
     markerResto1.bindPopup(`<b>${nom}</b><br>${adresse}`);
     var markerResto2 = L.marker([48.71, lng] );
+    markerResto2.on("click",() => console.log("test"));
+
 
     var markerVlib1 = L.marker([48.71, 6.21]);
     var markerVlib2 = L.marker([48.7, 6.21]);
@@ -49,9 +50,12 @@ export function init(){
     Vlib.addLayer(markerVlib2);
 }
 
-function addMarker(lat, lng, nom, adresse){
-    var marker = L.marker([lat, lng]).addTo(map);
+function addMarker(gps, id, nom, adresse){
+    let coordonnes = gps.splice(",");
+    var marker = L.marker([coordonnes[0], coordonnes[1]]);
     marker.bindPopup(`<b>${nom}</b><br>${adresse}`).openPopup();
+    Resto.addLayer(marker);
+    marker.on("click", () => /*{let resto = restaurant(gps,id,nom,adresse);}*/)
 }
 init();
 
