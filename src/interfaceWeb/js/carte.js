0ea56d8f9dd58011e64481a3a4a584fde39a3341
Julien Bernard <julienbernardjb88@gmail.com>
2023-06-15 14:07:12 +0200

    MarkerVlib

--- src/interfaceWeb/js/carte.js
+++ src/interfaceWeb/js/carte.js
@@ -7,7 +7,7 @@ const nancy = {
 const zoomLevel = 12;
 
 const GroupeMarkerResto = L.layerGroup([]);
-const Vlib = L.layerGroup([]);
+const GroupeMarkerVlib = L.layerGroup([]);
 
 
 export function init() {
@@ -24,13 +24,29 @@ export function init() {
 
     const SelecteurAffichage = {
         "restaurants": GroupeMarkerResto,
-        "Vlib": Vlib
+        "Vlib": GroupeMarkerVlib
     };
     L.control.layers(null, SelecteurAffichage).addTo(map);
 
+
+    //A supprimé par la suite
+    let lat = 48.7;
+    let lng = 6.2;
+    let nom = "La rivière";
+    let adresse = "Dans Nancy";
+    var markerResto1 = L.marker([lat, lng]);
+    markerResto1.bindPopup(`<b>${nom}</b><br>${adresse}`);
+    var markerResto2 = L.marker([48.71, lng] );
+    markerResto2.on("click",() => console.log("test"));
+    var markerVlib1 = L.marker([48.71, 6.21]);
+    var markerVlib2 = L.marker([48.7, 6.21]);
+    GroupeMarkerResto.addLayer(markerResto1);
+    GroupeMarkerResto.addLayer(markerResto2);
+    GroupeMarkerVlib.addLayer(markerVlib1);
+    GroupeMarkerVlib.addLayer(markerVlib2);
 }
 
-function addMarker(gps, id, nom, adresse){
+function addMarkerResto(gps, id, nom, adresse){
     console.log(gps);
     let coordonnes = gps.split(',');
     var marker = L.marker([coordonnes[0], coordonnes[1]]);
@@ -42,21 +58,28 @@ function addMarker(gps, id, nom, adresse){
     })
 }
 
+function addMarkerVlib(lat, lng, nom,capacite, nbDispo, adresse){
+    var marker = L.marker([lat,lng]);
+    marker.bindPopup(`<b>${nom}</b><br>${adresse}<br>Nombre velo dispo: ${nbDispo}/${capacite}`).openPopup();
+    GroupeMarkerVlib.addLayer(marker);
+}
+
+// var JsonObject
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "http://localhost:8000/votre-route", true);
+// xhr.onreadystatechange = function() {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+//         JsonObject = JSON.parse(xhr.response);
+//         console.log(JsonObject);
+//         for(let i = 0; i < JsonObject.restaurants.length; i++) {
+//             addMarkerResto(JsonObject.restaurants[i].GPS, JsonObject.restaurants[i].ID, JsonObject.restaurants[i].NOM, JsonObject.restaurants[i].ADRESSE);
+//         }
+//     } else {
+//         if (xhr.status !== 200)
+//             console.log("La requête a échoué. Code de réponse : " + xhr.status);
+//     }
+// };
+// xhr.send();
 init();
-var JsonObject
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://localhost:8000/votre-route", true);
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        JsonObject = JSON.parse(xhr.response);
-        console.log(JsonObject);
-        for(let i = 0; i < JsonObject.restaurants.length; i++) {
-            addMarker(JsonObject.restaurants[i].GPS, JsonObject.restaurants[i].ID, JsonObject.restaurants[i].NOM, JsonObject.restaurants[i].ADRESSE);
-        }
-    } else {
-        if (xhr.status !== 200)
-            console.log("La requête a échoué. Code de réponse : " + xhr.status);
-    }
-};
-xhr.send();
+
 
