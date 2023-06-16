841e870d3ed1dffdf30ec29a8ecbb5b400946f6c
alennardos <92914160+alennardos@users.noreply.github.com>
2023-06-16 15:26:22 +0200

    resa

--- src/interfaceWeb/js/uiReservation.js
+++ src/interfaceWeb/js/uiReservation.js
@@ -1,6 +1,8 @@
 import Restaurant from "./restaurant.js";
 
-function submitForm(resto) {
+let resto;
+
+function submitForm() {
     const nom = document.getElementById('nom').value;
     const prenom = document.getElementById('prenom').value;
     const nbConvives = document.getElementById('nbConvives').value;
@@ -11,49 +13,49 @@ function submitForm(resto) {
 
 function uiForm(restaurantt){
 
+    resto = restaurantt
+
     let node = document.createElement("div");
     document.querySelector("#affichageMap").appendChild(node);
     //node.setAttribute("id", "formReservation");
     node.classList.add("formReservation");
 
-    console.log('restaurant name : ' + restaurantt.nom)
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div id="informationRestoForm">
+          <h2>${restaurantt.nom}</h2>
+           <p>${restaurantt.adresse}</p>
+        </div>
+                  
+        <form id="reservationForm" >
+            
+            <h3>Réserver une table : </h3>
+            <div id="formulaireNom">
+                <label for="nom">Nom</label>
+                <input type="text" id="nom" required><br>
+            </div>
+            <div id="formulairePrenom">
+                 <label for="prenom">Prénom</label>
+                 <input type="text" id="prenom" required><br>
+            </div>
+            <div id="formulaireNbConv">
+                 <label for="nbConvives">Nombre de convives</label>
+                 <input type="number" id="nbConvives" required><br>
+            </div>
+            <div id="formulaireTelephone">
+                 <label for="telephone">Téléphone</label>
+                 <input type="tel" id="telephone" required><br>
+            </div>
+                <button type="button" id="submit">Réserver</button>
+        </form>`;
 
-    let html = `
-<div id="informationRestoForm">
-  <h2>${restaurantt.nom}</h2>
-   <p>${restaurantt.adresse}</p>
-</div>
-          
-<form id="reservationForm">
-    <h3>Réserver une table : </h3>
-    <div id="formulaireNom">
-        <label for="nom">Nom</label>
-        <input type="text" id="nom" required><br>
-    </div>
-    <div id="formulairePrenom">
-         <label for="prenom">Prénom</label>
-         <input type="text" id="prenom" required><br>
-    </div>
-    <div id="formulaireNbConv">
-         <label for="nbConvives">Nombre de convives</label>
-         <input type="number" id="nbConvives" required><br>
-    </div>
-    <div id="formulaireTelephone">
-         <label for="telephone">Téléphone</label>
-         <input type="tel" id="telephone" required><br>
-    </div>
-        <button type="submit" onclick="submitForm">Réserver</button>
-</form>`
-;
-
-node.innerHTML = html;
+    node.appendChild(div);
     setTimeout(function() {
         node.classList.add("show");
     }, 10);
 
     document.addEventListener("click", function(event) {
         const isClickedInsideNode = node.contains(event.target);
-        console.log('isClickedInsideNode : ' + isClickedInsideNode);
         if (!isClickedInsideNode && (event.target.toLocaleString() !== '[object HTMLImageElement]')) {
             node.classList.remove("show");
             setTimeout(function() {
@@ -61,6 +63,9 @@ node.innerHTML = html;
             }, 500); // Supprime la division après 500 millisecondes (0,5 seconde)
         }
     });
+
+    div.getElementsByTagName("button")[0].addEventListener("click",submitForm);
+
 }
 
 export default {
