b1394854f6f1f993c0fa9a4d9f45aade070bd4d8
Lucie <lucye.laury@gmail.com>
2023-06-19 10:49:10 +0200

    meteo recup de donnees

--- src/interfaceWeb/js/meteo.js
+++ src/interfaceWeb/js/meteo.js
@@ -1,31 +1,33 @@
 //récupérations et traitement des données météos
 let url = 'https://www.infoclimat.fr/public-api/gfs/json?_ll=48.67103,6.15083&_auth=ARsDFFIsBCZRfFtsD3lSe1Q8ADUPeVRzBHgFZgtuAH1UMQNgUTNcPlU5VClSfVZkUn8AYVxmVW0Eb1I2WylSLgFgA25SNwRuUT1bPw83UnlUeAB9DzFUcwR4BWMLYwBhVCkDb1EzXCBVOFQoUmNWZlJnAH9cfFVsBGRSPVs1UjEBZwNkUjIEYVE6WyYPIFJjVGUAZg9mVD4EbwVhCzMAMFQzA2JRMlw5VThUKFJiVmtSZQBpXGtVbwRlUjVbKVIuARsDFFIsBCZRfFtsD3lSe1QyAD4PZA%3D%3D&_c=19f3aa7d766b6ba91191c8be71dd1ab2';
 let listMeteo = [];
-
+let dateCourante = {
+};
 
 
 //8 prochaines données données
 async function getDataTemp(){
     const date = new Date();
-    const year = date.getFullYear();
-    let month = date.getMonth();
-    month++;
-    month = month < 10 ? '0'+month : month;
-    let day = date.getDay()
-    day = day < 10 ? '0'+day : day;
-    let currentHour = date.getHours(); // Récupère l'heure (de 0 à 23)
-    currentHour = currentHour < 10 ? '0'+currentHour : currentHour;
-    let currentMinute = date.getMinutes(); // Récupère les minutes (de 0 à 59)
-    currentMinute = currentMinute < 10 ? '0'+currentMinute : currentMinute;
-    let currentSecond = date.getSeconds();
-    currentSecond = currentSecond < 10 ? '0'+currentSecond : currentSecond;
+    dateCourante.year = date.getFullYear();
+    dateCourante.month = date.getMonth();
+    dateCourante.month++;
+    dateCourante.month = dateCourante.month < 10 ? '0'+dateCourante.month : dateCourante.month;
+    dateCourante.day = date.getDate();
+    dateCourante.day = dateCourante.day < 10 ? '0'+dateCourante.day : dateCourante.day;
+    dateCourante.hour = date.getHours(); // Récupère l'heure (de 0 à 23)
+    dateCourante.hour = dateCourante.hour < 10 ? '0'+dateCourante.hour : dateCourante.hour;
+    dateCourante.minute = date.getMinutes(); // Récupère les minutes (de 0 à 59)
+    dateCourante.minute = dateCourante.minute < 10 ? '0'+dateCourante.minute : dateCourante.minute;
+    dateCourante.second = date.getSeconds();
+    dateCourante.second = dateCourante.second < 10 ? '0'+dateCourante.second : dateCourante.second;
 
 
-    let totalCurrentDate = year + '-' + month + '-' + day + ' ' + currentHour + ':' + currentMinute  + ':' + currentSecond;
-    console.log('totalCurrentDate : ' + totalCurrentDate) ;
+    dateCourante.totalCurrentDate = dateCourante.year + '-' + dateCourante.month + '-' + dateCourante.day + ' ' +
+        dateCourante.hour + ':' + dateCourante.minute  + ':' + dateCourante.second;
+    console.log('totalCurrentDate : ' + dateCourante.totalCurrentDate) ;
     let response = await fetch(url)
         .then( data => {
-            getListTemp(data);
+            getListTemp( data.json());
             }
         )
         .catch(error =>{
@@ -35,9 +37,27 @@ async function getDataTemp(){
 
 }
 
-async function getListTemp(meteo){
+async function getListTemp(data){
+    let meteo = await data;
     console.log(meteo);
-    //console.log('meteo.temp.2m : ' + meteo.temperature.2m);
+    let currentDate = new Date(dateCourante.totalCurrentDate);
+    console.log(currentDate);
+    for(let elem in meteo){
+        let elemDate = new Date(elem);
+        if(!isNaN(elemDate)){
+            let comparaison = elemDate.getTime()<currentDate.getTime();
+
+            if(!comparaison){
+                if(listMeteo.length<8){
+                    listMeteo.push(elem);
+                }
+
+            }
+        }
+
+
+    }
+    console.log('meteo.temp.2m : ' + meteo['2023-06-26 20:00:00']);
 
 
 }
