612f810109ada5703d7c24713a1e4b35d4b06d40
Flotss <manginf54@gmail.com>
2023-06-20 17:02:58 +0200

    feat: HTTPS

--- src/ProxyServices/Serveur.java
+++ src/ProxyServices/Serveur.java
@@ -6,13 +6,19 @@ import ProxyServices.HTTPHandler.ServeurReservation;
 import ProxyServices.HTTPHandler.ServeurRestaurant;
 import app.proxy.ServiceProxyBlocageInterface;
 import app.restaurant.Service.ServiceRestaurantInterface;
-import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
+import com.sun.net.httpserver.HttpsServer;
 
+import javax.net.ssl.*;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.sql.SQLException;
 import java.util.Arrays;
 
@@ -67,26 +73,69 @@ public class Serveur {
     public static void main(String[] args) {
         if (args.length == 0) {
             args = new String[]{"localhost", "10002", "localhost", "10001"};
-        }else if (args.length < 4) {
+        } else if (args.length < 4) {
             System.out.println("Usage: java Serveur <adresseRestaurant> <portRestaurant> <adresseProxy> <portProxy>");
             System.exit(1);
         }
 
 
-        HttpServer server;
+        HttpsServer server;
         try {
-            server = HttpServer.create(new InetSocketAddress(8000), 0);
-        } catch (IOException e) {
+            // setup the socket address
+            InetSocketAddress address = new InetSocketAddress(8000);
+
+            // initialise the HTTPS server
+            server = HttpsServer.create(address, 0);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+
+            // initialise the keystore
+            char[] password = "password".toCharArray();
+            KeyStore ks = KeyStore.getInstance("JKS");
+            FileInputStream fis = new FileInputStream("src/ProxyServices/testkey.jks");
+            ks.load(fis, password);
+
+            // setup the key manager factory
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+
+            // setup the trust manager factory
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ks);
+
+            // setup the HTTPS context and parameters
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            server.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+                public void configure(HttpsParameters params) {
+                    try {
+                        // initialise the SSL context
+                        SSLContext context = getSSLContext();
+                        SSLEngine engine = context.createSSLEngine();
+                        params.setNeedClientAuth(false);
+                        params.setCipherSuites(engine.getEnabledCipherSuites());
+                        params.setProtocols(engine.getEnabledProtocols());
+
+                        // Set the SSL parameters
+                        SSLParameters sslParameters = context.getSupportedSSLParameters();
+                        params.setSSLParameters(sslParameters);
+
+                    } catch (Exception ex) {
+                        System.out.println("Failed to create HTTPS port");
+                    }
+                }
+            });
+        } catch (IOException | NoSuchAlgorithmException | KeyStoreException | UnrecoverableKeyException |
+                 KeyManagementException | CertificateException e) {
             throw new RuntimeException(e);
         }
 
         Serveur serveur = new Serveur(args[0], Integer.parseInt(args[1]),
-                                          args[2], Integer.parseInt(args[3]));
+                args[2], Integer.parseInt(args[3]));
 
         server.createContext("/api/restaurations", new ServeurRestaurant(serveur));
         server.createContext("/api/reservation", new ServeurReservation(serveur));
         server.createContext("/api/addRestaurant", new ServeurAjoutRestaurant(serveur));
         server.createContext("/api/proxy", new ServeurProxy(serveur));
+        server.setExecutor(null); // creates a default executor
         server.start();
 
 
