2288aed1d6f3fc14d4d9a01d39008eb0d573f5cd
bernar386u <julienbernardjb88@gmail.com>
2023-06-21 11:32:38 +0200

    Patch: etablissement/ ajout HTTPCLient

--- src/app/proxy/ServiceProxyBlocage.java
+++ src/app/proxy/ServiceProxyBlocage.java
@@ -1,81 +1,68 @@
 package app.proxy;
 
-import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.rmi.RemoteException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.rmi.RemoteException;
-import java.security.cert.X509Certificate;
 
 public class ServiceProxyBlocage implements ServiceProxyBlocageInterface {
 
-
     public String makeRequest(String url) throws RemoteException {
+        System.out.println(url);
         try {
-            // Ajout du protocole à l'URL
-            if (!url.startsWith("http://") && !url.startsWith("https://")) {
-                url = "http://" + url;
-            }
-
-            // Si l'URL utilise HTTPS, on ajoute le certificat à la liste des certificats de confiance
+            ProxySelector proxySelector = ProxySelector
+                    .of(new InetSocketAddress("www-cache.iutnc.univ-lorraine.fr", 3128));
             SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, new TrustManager[]{new X509TrustManager() {
-                public void checkClientTrusted(X509Certificate[] chain, String authType) {
+            sslContext.init(null, new TrustManager[] { new X509TrustManager() {
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
                 }
 
-                public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
                 }
 
                 public X509Certificate[] getAcceptedIssuers() {
                     return new X509Certificate[0];
                 }
-            }}, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+            } }, new SecureRandom());
+            HttpClient client = HttpClient.newBuilder()
+                    .proxy(proxySelector)
+                    .sslContext(sslContext)
+                    .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Origin", "https://webetu.iutnc.univ-lorraine.fr")
+                    .build();
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Requête HTTP envoyée à " + url);
 
-            // Ajout du certificat de confiance
-            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+            System.out.println(response.body());
+            System.out.println("response = " + response);
 
 
-            // Création de l'objet URL à partir de l'URL spécifiée
-            URL urlObject = new URL(url);
-
-            // Ouverture de la connexion HTTP
-            HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
-
-            // Spécification de la méthode de requête (GET dans ce cas)
-            connection.setRequestMethod("GET");
-
-
-            // Lecture de la réponse de la requête
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            StringBuilder response = new StringBuilder();
-
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-
-            // Fermeture des ressources
-            reader.close();
-            connection.disconnect();
-
-            // Envoi de la réponse
-            int code = connection.getResponseCode();
+// Envoi de la réponse
+            int code = response.statusCode();
             String status = (code == 200) ? "OK" : "ERROR";
 
-
-            return "{ \"status\": \"" + status + "\", \"code\": " + code + ", \"response\": " + response + " }";
+            return "{ \"status\": \"" + status + "\", \"code\": " + code + ", \"response\": " + response.body() + " }";
         } catch (IOException e) {
             System.out.println("Erreur lors de la requête HTTP: \n" + e.getMessage());
-            return "{ status: \"ERROR\", code: 500, response: \"Erreur lors de la requête HTTP : " + e.getMessage() + "\" }";
+            return "{ status: \"ERROR\", code: 500, response: \"Erreur lors de la requête HTTP : " + e.getMessage()
+                    + "\" }";
         } catch (Exception e) {
             System.out.println("Erreur lors de l'ajout du certificat de confiance: \n" + e.getMessage());
-            return "{ status: \"ERROR\", code: 500, response: \"Erreur lors de l'ajout du certificat de confiance : " + e.getMessage() + "\" }";
+            return "{ status: \"ERROR\", code: 500, response: \"Erreur lors de l'ajout du certificat de confiance : "
+                    + e.getMessage() + "\" }";
         }
     }
 }
